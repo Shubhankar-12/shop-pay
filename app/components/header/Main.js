65d192a843388b48f1b51f04adf163f0ce74946c
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-08-12 12:14:54 +0530

    main created

---
+++ app/components/header/Main.js
@@ -0,0 +1,34 @@
+import Link from 'next/link';
+import styles from './styles.module.scss';
+import { RiSearch2Line } from 'react-icons/ri';
+import { FaOpencart } from 'react-icons/fa';
+import { useSelector } from 'react-redux';
+
+const Main = () => {
+    const { cart } = useSelector((state) => ({ ...state }));
+    return (
+        <div className={styles.main}>
+            <div className={styles.main__container}>
+                <Link href="/">
+                    <div className={styles.logo}>
+                        <img src='../../../logo.png' />
+                    </div>
+                </Link>
+                <div className={styles.search}>
+                    <input type='text' placeholder='Search...' />
+                    <div className={styles.search__icon}>
+                        <RiSearch2Line />
+                    </div>
+                </div>
+                <Link href="/cart">
+                    <div className={styles.cart}>
+                        <FaOpencart />
+                        <span>{cart.length}</span>
+                    </div>
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default Main
