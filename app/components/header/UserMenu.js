1bb1341fb70a2d9412d2e2701d49df5183a9b383
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-08-12 12:15:10 +0530

    usermenu done

--- app/components/header/UserMenu.js
+++ app/components/header/UserMenu.js
@@ -1,8 +1,37 @@
+import Link from 'next/link';
 import styles from './styles.module.scss';
 
-const UserMenu = () => {
+const UserMenu = ({ loggedIn }) => {
     return (
-        <div className={styles.menu}></div>
+        <div className={styles.menu}>
+            <h4>Welcome to Shoppay !</h4>
+            {loggedIn ? (
+                <div className={styles.flex}>
+                    <img
+                        src="https://github.com/Shubhankar-12/shop-pay/blob/main/public/images/userphoto.png?raw=true"
+                        alt=''
+                        className={styles.menu__img}
+                    />
+                    <div className={styles.col}>
+                        <span>Welcome Back,</span>
+                        <h3>Shubh</h3>
+                        <span>Sign Out</span>
+                    </div>
+                </div>
+            ) : (
+                <div className={styles.flex}>
+                    <button className={styles.btn_primary}>Register</button>
+                    <button className={styles.btn_outlined}>login</button>
+                </div>
+            )}
+            <ul>
+                <li><Link href="/profile">Account</Link></li>
+                <li><Link href="/profile/orders">My Orders</Link></li>
+                <li><Link href="/profile/messages">Messages</Link></li>
+                <li><Link href="/profile/address">Address</Link></li>
+                <li><Link href="/profile/wishlist">Wishlist</Link></li>
+            </ul>
+        </div>
     )
 }
 
