fc99e2b397847c3ee1ea3b42c4d05c52f781ff24
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-08-24 13:14:05 +0530

    footer done

--- app/components/footer/index.js
+++ app/components/footer/index.js
@@ -1,9 +1,23 @@
-import './styles.module.scss';
+import Copyright from './Copyright';
+import Links from './Links';
+import NewsLetter from './NewsLetter';
+import Payment from './Payment';
+import Socials from './Socials';
+import styles from './styles.module.scss';
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <footer className={styles.footer}>
+            <div className={styles.footer__container}>
+                <Links />
+                <Socials />
+                <NewsLetter />
+                <Payment />
+                <Copyright />
+            </div>
+        </footer>
+
     )
 }
 
-export default Footer
+export default Footer;
