acf42e66fa91186c4697cf38a821d133a156694b
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-10-04 12:04:15 +0530

    Product schema dome

--- models/product.js
+++ models/product.js
@@ -127,7 +127,6 @@ const productSchema = new mongoose.Schema(
         timestamps: true,
     }
 );
-const Product =
-    mongoose.models.Product || mongoose.model("Product", productSchema);
+const Product = mongoose.models.Product || mongoose.model("Product", productSchema);
 
 export default Product;
