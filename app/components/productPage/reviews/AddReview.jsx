81c3363ca54e64c2820e3c9e24dc63b69df06e63
Shubhankar-12 <shubh.shubhankar2020@vitbhopal.ac.in>
2023-10-16 12:06:09 +0530

    user rating and review

--- app/components/productPage/reviews/AddReview.jsx
+++ app/components/productPage/reviews/AddReview.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import Select from './Select'
 import styles from './styles.module.scss'
+import { Rating } from '@mui/material';
 
 let fits = ["Small", "True to size", "Large"];
 const AddReview = ({ product }) => {
     const [size, setSize] = useState("");
     const [style, setStyle] = useState("");
     const [fit, setFit] = useState("");
+    const [review, setReview] = useState("");
+    const [rating, setRating] = useState();
+
     const handleSize = (size) => {
         setSize(size);
     }
@@ -36,6 +40,23 @@ const AddReview = ({ product }) => {
                         handleChange={setFit}
                     />
                 </div>
+                <textarea
+                    name="review"
+                    value={review}
+                    onChange={(e) => setReview(e.target.value)}
+                    placeholder='Write your review here...'
+                />
+                <Rating
+                    name="half-rating-read"
+                    defaultValue={0}
+                    value={rating}
+                    onChange={(e) => setRating(e.target.value)}
+                    precision={0.5}
+                    style={{ color: "#facf19", fontSize: "3rem" }}
+                />
+                <button className={styles.login_btn}>
+                    Submit Review
+                </button>
             </div>
         </div>
     )
